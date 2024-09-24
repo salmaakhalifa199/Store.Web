078bbb536606dc44d74f997ff871679d25e0cdbc
salmaakhalifa199 <salmakhalifa06991@gmail.com>
2024-09-24 19:42:42 +0300

    Add project files.

---
+++ Store.Repository/Interfaces/IUnitWork.cs
@@ -0,0 +1,16 @@
+﻿using Store.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Repository.Interfaces
+{
+    public interface IUnitWork
+    {
+        // h3ml fun. hya ali htkon masola 3la anha taagbli objext mn el haga ali btlobha
+        IGenericRepository<TEntity,Tkey> Repository<TEntity, Tkey>() where TEntity : BaseEntity<Tkey>;
+        Task<int> CompleteAsync();
+    }
+}
---
+++ Store.Services/Services/ProductServices/Dtos/ProductPictureUrlResolver.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Store.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Services.Services.ProductServices.Dtos
+{
+    public class ProductPictureUrlResolver : IValueResolver<Product, ProductDetailsDto, string>
+    {
+        private readonly IConfiguration _configuration;
+
+        // a'ra el data mn el appsettings
+        public ProductPictureUrlResolver(IConfiguration configuration )
+        {
+            _configuration = configuration;
+        }
+        public string Resolve(Product source, ProductDetailsDto destination, string destMember, ResolutionContext context)
+        {
+            if(!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return $"{_configuration["BaseUrl"]}{source.PictureUrl}";
+            }
+            return null;
+        }
+    }
+}
